f693481773829636980b770fb2bfd335202d6b6d
Phuoc Phan <phanminh_phuoc@yahoo.com>
2021-06-22 02:40:54 +0700

    finish baitap redux

---
+++ baitap-redux/src/Data/data.js
@@ -0,0 +1,26 @@
+const data = {
+    products: [
+        {
+            id:'1',
+            name:"Playstation 5",
+            price: 700,
+            image: "https://anphat.com.vn/media/product/33475_sony_playstation_5.jpg"
+
+        },
+        {
+            id:'2',
+            name:"Nintendo Switch",
+            price: 400,
+            image: "https://images-na.ssl-images-amazon.com/images/I/61-PblYntsL._AC_SL1500_.jpg"
+
+        },
+        {
+            id:'3',
+            name:"TV 50",
+            price: 1400,
+            image: "https://ae01.alicdn.com/kf/H0cba72b9cbc44a56acff9532a293b436n/Wifi-Android-Smart-Tivi-LED-50-Inch-LED-LCD-Truy-n-H-nh.jpg_Q90.jpg_.webp"
+
+        },
+    ],
+};
+export default data;
